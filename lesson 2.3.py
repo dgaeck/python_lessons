5aba857c333420e07585bd7570597b5ed5289741
dgaeck <dgaeck@myfairpoint.net>
2019-03-17 10:36:52 -0400

    lesson 2.3

--- lesson 2.3.py
+++ lesson 2.3.py
@@ -1,17 +1,7 @@
-name = input('Enter file:')
-handle = open(name, 'r')
-counts = dict()
+# This first line is provided for you
 
-for line in handle:
-    words = line.split()
-    for word in words:
-        counts[word] = counts.get(word, 0) + 1
+hrs = input("Enter Hours:")
+rate = input("Enter Rate:")
+pay = float(rate) * float(hrs)
 
-bigcount = None
-bigword = None
-for word, count in list(counts.items()):
-    if bigcount is None or count > bigcount:
-        bigword = word
-        bigcount = count
-
-print(bigword, bigcount)
+print(pay)
