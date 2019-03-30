17deed30c0844b435a580b025ef4d9d8442e4786
dgaeck <dgaeck@myfairpoint.net>
2019-03-30 14:14:11 -0400

    lesson 4.6

---
+++ lesson 4.6.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 30 14:02:04 2019
+
+@author: User
+"""
+
+def computepay(h,r):
+    return 42.37
+
+hrs = input("Enter Hours:")
+p = computepay(10,20)
+print("Pay",p)
